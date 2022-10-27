6834d678007dbc633e97afe933b01c791b9cb1f6
Hana Bajzikova <hanagavu@gmail.com>
2022-10-27 22:04:28 +0200

    Added list print_usage function which shows CLI arguments when the program runs without any arguments.

--- todo.py
+++ todo.py
@@ -3,18 +3,13 @@ import datetime
 
 
 def print_usage():
-    info = '\nCommand Line Todo application\n' \
-           '=============================\n' \
-           'Command line arguments:\n' \
-           '-l   Lists all the tasks\n' \
-           '-a   Adds a new task\n' \
-           '-r   Removes a task\n' \
-           '-c   Completer a task'
+    info = '\nCommand Line Todo application\n=============================\nCommand line arguments:\n-l   Lists all ' \
+           'the tasks\n-a   Adds a new task\n-r   Removes a task\n-c   Completer a task '
 
-    sys.stdout.write(info.encode('utf8'))
+    sys.stdout.write(info)
 
 
 if __name__ == '__main__':
     args = sys.argv
-    if len(args) == 0:
+    if len(args) == 1:
         print_usage()
