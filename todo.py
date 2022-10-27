8ae2d989eaacd9f0884be17a93eb5cba5e9db2f6
Hana Bajzikova <hanagavu@gmail.com>
2022-10-27 22:46:50 +0200

    Add task function added. Usable with -a command and the task decription as an argument

--- todo.py
+++ todo.py
@@ -10,10 +10,31 @@ def print_usage():
 
 
 def list_tasks():
-    
+    with open("list_of_tasks.txt", 'r') as fobj:
+        fobj_content = fobj.readlines()
+        if len(fobj_content) == 0:
+            sys.stdout.write('No todos for today! :)')
+
+        else:
+            task_num = 1
+            for line in fobj_content:
+                sys.stdout.write(f'{task_num} - {line}')
+                task_num += 1
+
+
+def add_new_task(task):
+    with open("list_of_tasks.txt", "a") as fobj:
+        fobj.write('\n' + task)
+        sys.stdout.write(f'New task: {task} has been added')
 
 
 if __name__ == '__main__':
     args = sys.argv
     if len(args) == 1:
         print_usage()
+
+    elif args[1] == '-l':
+        list_tasks()
+
+    elif args[1] == '-a':
+        add_new_task(args[2])
