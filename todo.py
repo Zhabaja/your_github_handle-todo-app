7b70a11194cdee23a942a5a94d828efc5822fffc
Hana Bajzikova <hanagavu@gmail.com>
2022-10-28 22:02:47 +0200

    Remove task (-r) function added, remove task function error handling added, argument error handling added

--- todo.py
+++ todo.py
@@ -31,9 +31,31 @@ def add_new_task(task=''):
             sys.stdout.write(f'New task: {task} has been added')
 
 
+# saves the tasks in the list_of_tasks.txt file into a list variable
+def task_list():
+    with open("list_of_tasks.txt", 'r') as fobj:
+        task_list = fobj.readlines()
+        return task_list
+
+
+def remove_task(index=0):
+    task_ls = task_list()
+    sys.stdout.write(f'Task: {task_ls[index - 1]} has been deleted.')
+    del task_ls[index - 1]
+    with open("list_of_tasks.txt", "w") as fobj:
+        for task in task_ls:
+            fobj.write(task)
+
+
 if __name__ == '__main__':
     args = sys.argv
+    valid_args = ['-l', '-a', '-r', 'c']
+
     if len(args) == 1:
+        print_usage()
+
+    elif args[1] not in valid_args:
+        sys.stderr.write('\nUnsupported argument\n')
         print_usage()
 
     elif args[1] == '-l':
@@ -46,3 +68,13 @@ if __name__ == '__main__':
         except IndexError:
             add_new_task()
 
+    elif args[1] == '-r':
+        try:
+            remove_task(int(args[2]))
+        except ValueError:
+            sys.stderr.write("Unable to remove: index is not a number")
+        except IndexError:
+            if len(args) < 3:
+                sys.stderr.write('Unable to remove: no index provided')
+            elif int(args[2]) - 1 > len(task_list()):
+                sys.stderr.write('Unable to remove: index is out of bound')
